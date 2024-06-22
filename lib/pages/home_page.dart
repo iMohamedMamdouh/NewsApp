c747c1fae7af1d5d781a8a741a776d56aea7c8f8
iMohamedMamdouh <imohamedmamdoh1@gmail.com>
2024-06-22 08:18:36 +0200

    create widgets folder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:demo_newsapp/pages/row_card.dart';
+import 'package:demo_newsapp/widgets/row_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
